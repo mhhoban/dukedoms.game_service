f1f9b761c651a11a8863f4c0e91bb0e422a7cb34
mhhoban <mhhoban@gmail.com>
2018-03-04 16:47:18 -0800

    added json response with creation status and game id

--- game_service/__init__.py
+++ game_service/__init__.py
@@ -1,7 +1,10 @@
+import json
+
 from flask import Flask, request
-from game_service.shared.db import session, init_db
+from sqlalchemy.exc import SQLAlchemyError
 
 from game_service.models.game import Game
+from game_service.shared.db import session, init_db
 
 init_db()
 app = Flask(__name__)
@@ -16,4 +19,13 @@ def create_new_game():
     game_state = 'pending'
     new_game = Game(game_state=game_state, players=players)
     session.add(new_game)
-    session.commit()
+
+    try:
+        session.commit()
+        status = {'game_created': True, 'game_id': new_game.id}
+
+    except SQLAlchemyError:
+        status = {'game_created': False}
+
+    status = json.dumps(status)
+    return status
