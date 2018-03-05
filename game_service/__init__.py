d234e7c35c6c386813965395598fc7254d1aec50
mhhoban <mhhoban@gmail.com>
2018-03-04 17:21:53 -0800

    moved validator method and added unit tests for validators

--- game_service/__init__.py
+++ game_service/__init__.py
@@ -3,6 +3,7 @@ import json
 from flask import Flask, request
 from sqlalchemy.exc import SQLAlchemyError
 
+from game_service.data_validation.data_validators import validate_new_game_request
 from game_service.models.game import Game
 from game_service.shared.db import session, init_db
 
@@ -15,6 +16,9 @@ def create_new_game():
     Endpoint for creating new game
     """
     players = request.get_json()
+
+    if not validate_new_game_request(players):
+        return json.dumps({'game_created': False, 'reason': 'missing players'})
 
     game_state = 'pending'
     new_game = Game(game_state=game_state, players=players)
